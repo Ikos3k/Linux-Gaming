fa3ecd42b0b9816a0d3d804a8234abd9428e2fee
Ikos3k <ikosek.zt@gmail.com>
2024-11-25 17:30:54 +0100

    sway env support

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Useful scripts for gamers on Linux
-[launch_cs2.sh](https://github.com/Ikos3k/Linux-Gaming/blob/master/launch_cs2.sh) - Launch Counter-Strike 2 with custom resolution and refresh rate for X11 environments.
+[launch_cs2.sh](https://github.com/Ikos3k/Linux-Gaming/blob/master/launch_cs2.sh) - Launch Counter-Strike 2 with custom resolution and refresh rate for X11 & Sway (Wayland) environments.
--- launch_cs2.sh
+++ launch_cs2.sh
@@ -2,6 +2,9 @@
 
 TARGET_RESOLUTION="1280x960"
 TARGET_REFRESH_RATE=165
+TARGET_SCALE_MODE="Default" # Options: Full, Aspect, Center, Default
+TARGET_FILTER="Default"     # Options: nearest, bilinear, default
+NOTIFICATIONS=0
 
 GAME_PROCESS_NAME="cs2"
 APP_ID=730
@@ -9,14 +12,35 @@ APP_ID=730
 WIDTH=$(echo "$TARGET_RESOLUTION" | cut -d'x' -f1)
 HEIGHT=$(echo "$TARGET_RESOLUTION" | cut -d'x' -f2)
 
-LAUNCH_OPTIONS="-novid -nojoy -high -console -w $WIDTH -h $HEIGHT -fullscreen +r_dynamic 0 +fps_max $TARGET_REFRESH_RATE"
+LAUNCH_OPTIONS="-novid -nojoy -high -console -noaafonts -nosync -noipx -freq $TARGET_REFRESH_RATE -refresh $TARGET_REFRESH_RATE -w $WIDTH -h $HEIGHT -fullscreen +fps_max $TARGET_REFRESH_RATE -forcenovsync"
 
-if [ -z "$DISPLAY" ]; then
-    echo "Not running in X11 environment. Exiting."
+SESSION_TYPE="Unknown"
+if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+    SESSION_TYPE="Wayland"
+    if pgrep -x "sway" > /dev/null; then
+        echo "Sway (Wayland) is in use."
+        SESSION_TYPE="Wayland_Sway"
+    else
+        echo "Wayland is in use, but not Sway. Not supported environment."
+        exit 1
+    fi
+elif [ -n "$DISPLAY" ]; then
+    SESSION_TYPE="X11"
+    echo "X11 is in use."
+    if command -v notify-send >/dev/null 2>&1; then
+        NOTIFICATIONS=1
+    fi
+else
+    echo "Not supported environment."
     exit 1
 fi
 
-notify-send "Game Notification" "CS2 is starting!" --icon=dialog-information
+echo "Session type: $SESSION_TYPE"
+
+if [ "$NOTIFICATIONS" -eq 1 ]; then
+    notify-send "Game Notification" "CS2 is starting!" --icon=dialog-information
+fi
+
 steam -silent -vgui -applaunch $APP_ID $LAUNCH_OPTIONS &
 
 while ! GAME_PID=$(pgrep -x "$GAME_PROCESS_NAME"); do
@@ -24,24 +48,43 @@ while ! GAME_PID=$(pgrep -x "$GAME_PROCESS_NAME"); do
 done
 
 echo "CS2 process detected. Waiting for it to end..."
-DISP_OUT=$(xrandr | grep " connected" | awk '{print $1}')
-CURRENT_RESOLUTION=$(xrandr | grep -A1 "^$DISP_OUT connected" | tail -n1 | awk '{ print $1 }')
-TARGET_MODE_NAME="${TARGET_RESOLUTION}_${TARGET_REFRESH_RATE}.00"
-TARGET_MODELINE=$(cvt $(echo $TARGET_RESOLUTION | tr 'x' ' ') $TARGET_REFRESH_RATE.00 | grep "Modeline" | cut -d' ' -f3-)
 
-if xrandr --verbose | grep -q "$TARGET_MODE_NAME"; then
-    echo "Mode $TARGET_MODE_NAME already exists."
-else
-    echo "Adding mode $TARGET_MODE_NAME..."
-    xrandr --newmode "$TARGET_MODE_NAME" $TARGET_MODELINE
-    xrandr --addmode $DISP_OUT "$TARGET_MODE_NAME"
+if [ "$SESSION_TYPE" == "Wayland_Sway" ]; then
+    DISP_OUT=$(swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .name')
+    CURRENT_RESOLUTION=$(swaymsg -t get_outputs | jq -r --arg DISP "$DISP_OUT" '.[] | select(.name == $DISP) | "\(.current_mode.width)x\(.current_mode.height)"')
+    echo "Adding custom mode $TARGET_RESOLUTION@$TARGET_REFRESH_RATE on $DISP_OUT..."
+    swaymsg -- output $DISP_OUT mode --custom $TARGET_RESOLUTION@$TARGET_REFRESH_RATE"Hz"
+elif [ "$SESSION_TYPE" == "X11" ]; then
+    DISP_OUT=$(xrandr | grep " connected" | awk '{print $1}')
+    CURRENT_RESOLUTION=$(xrandr | grep -A1 "^$DISP_OUT connected" | tail -n1 | awk '{ print $1 }')
+    TARGET_MODE_NAME="${TARGET_RESOLUTION}_${TARGET_REFRESH_RATE}.00"
+    TARGET_MODELINE=$(cvt $(echo $TARGET_RESOLUTION | tr 'x' ' ') $TARGET_REFRESH_RATE.00 | grep "Modeline" | cut -d' ' -f3-)
+    
+    if xrandr --verbose | grep -q "$TARGET_MODE_NAME"; then
+        echo "Mode $TARGET_MODE_NAME already exists."
+    else
+        echo "Adding custom mode $TARGET_RESOLUTION@$TARGET_REFRESH_RATE on $DISP_OUT..."
+        xrandr --newmode "$TARGET_MODE_NAME" $TARGET_MODELINE
+        xrandr --addmode $DISP_OUT "$TARGET_MODE_NAME"
+    fi
+    
+    if [ "$TARGET_SCALE_MODE" != "Default" ]; then
+      echo "Setting scaling mode to $TARGET_SCALE_MODE on $DISP_OUT..."
+      xrandr --output $DISP_OUT --set "scaling mode" "$TARGET_SCALE_MODE"
+    else
+      echo "Scaling mode is set to Default. No changes made."
+    fi
+    
+    if [ "$TARGET_FILTER" != "Default" ]; then
+      echo "Setting scaling filter to $TARGET_FILTER on $DISP_OUT..."
+      xrandr --output $DISP_OUT --filter "$TARGET_FILTER"
+    else
+      echo "Scaling filter is set to Default. No changes made."
+    fi
+    
+    echo "Setting mode $TARGET_MODE_NAME on $DISP_OUT..."
+    xrandr --output $DISP_OUT --mode "$TARGET_MODE_NAME"
 fi
-
-echo "Setting scaling mode to Full on $DISP_OUT..."
-xrandr --output $DISP_OUT --set "scaling mode" "Full"
-
-echo "Setting mode $TARGET_MODE_NAME on $DISP_OUT..."
-xrandr --output $DISP_OUT --mode "$TARGET_MODE_NAME"
 
 while kill -0 "$GAME_PID" 2> /dev/null; do
     sleep 10
@@ -49,7 +92,13 @@ done
 
 echo "CS2 process has ended."
 echo "Restoring original res $CURRENT_RESOLUTION on $DISP_OUT..."
-xrandr --output $DISP_OUT --mode $CURRENT_RESOLUTION --rate 60 --primary
+if [ "$SESSION_TYPE" == "Wayland_Sway" ]; then
+    swaymsg -- output $DISP_OUT mode $CURRENT_RESOLUTION
+elif [ "$SESSION_TYPE" == "X11" ]; then
+    xrandr --output $DISP_OUT --mode $CURRENT_RESOLUTION --rate 60
+fi
 
-sleep 5
-notify-send "Game Notification" "CS2 has closed!" --icon=dialog-information
+if [ "$NOTIFICATIONS" -eq 1 ]; then
+    sleep 5
+    notify-send "Game Notification" "CS2 has closed!" --icon=dialog-information
+fi
